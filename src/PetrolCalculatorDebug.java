7352111eddd1708b979fb9bacdc1d3c73d31c59e
Volan-de-krot <115557216+kv-zayceva@users.noreply.github.com>
2023-03-30 17:44:36 +0300

    PetrolCalculatorDebug.java
    Vnesla esp

--- src/PetrolCalculatorDebug.java
+++ src/PetrolCalculatorDebug.java
@@ -7,25 +7,25 @@ public class PetrolCalculatorDebug {
 
     public void calculate() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter fuel type: 1 — 92 octane gas, 2 — 95 octane gas, 3 — 100 octane gas => ");
+        System.out.print("Ingresa el tipo de combustible: 1 — gasolina de 92 octanos, 2 — gasolina de 95 octanos, 3 — gasolina de 100 octanos => ");
         int petrolType = scanner.nextInt();
-        System.out.print("Enter the number of liters => ");
+        System.out.print("Introduce el número de litros => ");
         double volume = scanner.nextDouble();
-        System.out.print("Enter a discount in the range of 0..100 => ");
+        System.out.print("Introduce un descuento en el rango de 0..100 => ");
         double discount = scanner.nextDouble() / 100;
 
         if (petrolType == 1) {
             Ai92Calculator calculator = new Ai92Calculator();
             double price = calculator.calculate(volume, discount);
-            System.out.println("The price of " + volume + " liters of the 92 octane gas is " + price + "$");
+            System.out.println("El costo de " + volume + " litros de gasolina de 92 octanos es $" + price");
         } else if (petrolType == 2) {
             Ai95Calculator calculator = new Ai95Calculator();
             double price = calculator.calculate(volume, discount);
-            System.out.println("The price of " + volume + " liters of the 95 octane gas is = " + price + "$");
+            System.out.println("El costo de " + volume + " litros de gasolina de 95 octanos es $" + price");
         } else if (petrolType == 3) {
             Ai100Calculator calculator = new Ai100Calculator();
             double price = calculator.calculate(volume, discount);
-            System.out.println("The price of " + volume + " liters of the 100 octane gas is = " + price + "$");
+            System.out.println("El costo de " + volume + " litros de gasolina de 100 octanos es $" + price");
         }
     }
 }
